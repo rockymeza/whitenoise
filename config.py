cab298d310f3150967ab059896ec97e8342f5945
Colin Thomas-Arnold <colinta@mac.com>
2012-01-17 23:56:17 -0700

    rm example processor

--- config.py
+++ config.py
@@ -11,22 +11,5 @@ ENVIRONMENT.filters['markdown2'] = markdown2
 
 CONFIG = {
     'time': int(time()),
-    'now': datetime.today()
+    'now': datetime.today(),
 }
-
-
-from processor import Processor
-from node import JinjaNode
-
-
-def my_page_processor(config, source_path, target_path, public_path):
-    hidden_config = {}
-    hidden_config.update(config)
-    hidden_config['name'] = "_%s_hidden" % config['name']
-    return (
-        JinjaNode(config, public_path, source_path, target_path),
-        JinjaNode(hidden_config, "%s.hidden" % public_path, source_path, "%s.hidden" % target_path),
-        )
-
-
-Processor.register('hidden_page', my_page_processor)
